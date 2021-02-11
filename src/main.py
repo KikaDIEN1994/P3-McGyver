5c8f7820e67164533b77760fe360dd2bc71e182a
KikaDIEN1994 <52467687+KikaDIEN1994@users.noreply.github.com>
2021-02-11 09:29:48 +0100

    Print maze

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
-# from maze import read_maze
+import os
+
 import maze
 
-maze.read_maze("assets\level.txt")
+print(os.path.dirname(__file__))
+
+file_path = os.path.join(os.path.dirname(__file__), "assets", "level.txt")
+
+print(file_path)
+
+maze.read_maze(file_path)
--- src/maze.py
+++ src/maze.py
@@ -1,5 +1,12 @@
 def read_maze(file):
-  print(f"Opening file : {file}")
-  with open(file) as f:
-    for line in f:
-      print(line.strip())
+    print(f"Opening file : {file}")
+    with open(file) as reader:
+        # print(reader.readlines())
+        for line in reader.readlines():
+            line = line.strip()
+            print(line)
+
+    # with open(file) as reader:
+    #   print(reader.readlines())
+    # for line in f:
+    #   print(line.strip())
