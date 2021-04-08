2c828379a044e261c4f67b814eab1d01cda367b8
KikaDIEN1994 <dien.kika@gmail.com>
2021-04-08 09:29:21 +0200

    Basic pygame loop

--- src/app.py
+++ src/app.py
@@ -1,6 +1,44 @@
 import os
 
+import pygame
+
 from .maze import Maze
+class Game:
+    def __init__(self):
+        self._running = True
+        self._screen = None
+        self.size = self.width, self.height = 640, 400
+
+    def on_init(self):
+        pygame.init()
+        self._screen = pygame.display.set_mode(
+            self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self._running = True
+
+    def on_event(self, event):
+        print("on event")
+        if event.type == pygame.QUIT:
+            self._running = False
+
+    def on_loop(self):
+        pass
+
+    def on_render(self):
+        pass
+
+    def on_cleanup(self):
+        pygame.quit()
+
+    def on_execute(self):
+        if self.on_init():
+            self._running = False
+
+        while(self._running):
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.on_loop()
+            self.on_render()
+        self.on_cleanup()
 
 
 class App:
@@ -15,3 +53,6 @@ class App:
 
         print(Maze.random_empty_tile())
         Maze.print()
+
+        game = Game()
+        game.on_execute()
